3724e1f438b0d27691fe35abf9e78e850fe3e3ab
Amit Rai <techamitn@gmail.com>
2019-08-29 23:39:05 +0530

    sweden translation

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var api = "AIzaSyC6OBaxECFDGHejevOtFC6RmUCrKFTYTXQ";
 var googleTranslate = require('google-translate')(api);
 
-var text = 'I am using google translator to convert this text to spanish'
+var text = 'I am using google translator to convert this text to swedish'
 console.log("English :>",text);
-googleTranslate.translate(text, 'es', function(err, translation) {
+googleTranslate.translate(text, 'sv', function(err, translation) {
   console.log("Spanish :>",translation.translatedText);
 });								
